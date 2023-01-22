274f89b58d960b5638bd2fc705fc238acddf8d1c
qwazar14 <zenez89@gmail.com>
2023-01-22 17:36:44 +0200

    a

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ class ScotlandCities:
 
         self.cities = ["Aberdeen", "Edinburgh", "Glasgow", "Inverness", "Dundee", "Stirling", "Perth"]
 
-
     def create_dataframe(self):
         # RDD зі списку міст
         cities_rdd = self.spark.sparkContext.parallelize(self.cities)
@@ -30,62 +29,50 @@ class ScotlandCities:
         # Перетворимо RDD в DataFrame
         self.cities_df = self.spark.createDataFrame(cities_lengths_rdd, ["city", "length"])
 
-
     def show_result(self):
         self.cities_df.show()
-
 
     def count_rows(self):
         # Рахуємо кількість рядків у фреймі даних
         print("Number of rows: ", self.cities_df.count())
 
-
     def delete_column(self):
         # Видаляємо стовпець з назвою "length"
         self.cities_df = self.cities_df.drop("length")
-
 
     def convert_to_json(self):
         # Перетворимо формат DataFrame у формат JSON
         cities_json = self.cities_df.toJSON().collect()
         print(cities_json)
 
-
     def filter_rows(self):
         # Залишимо тільки рядки стовпця "city" з довжиною 4 або 5
         self.cities_df = self.cities_df.filter((length(col("city")) == 4) | (length(col("city")) == 5))
-
 
     def rename_column(self):
         # Вибираємо стовпець "city", перейменуємо його на "mid_length"
         self.cities_df = self.cities_df.selectExpr("city as mid_length")
 
-
     def delete_row(self):
         # Видаляємо рядок зі словом "Dundee"
         self.cities_df = self.cities_df.filter(col("mid_length") != "Dundee")
-
 
     def sort_alphabetically(self):
         # Відсортуймо назви міст за алфавітом
         self.cities_df = self.cities_df.sort("mid_length")
 
-
     def filter_and_sort_by_length(self):
         # Збережемо назви міст з кількістю літер у назві >= 6 і відсортуємо назви міст за зростанням довжини назви
         self.cities_df = self.cities_df.filter(length("mid_length") >= 6).sort(length("mid_length"), ascending=True)
-
 
     def pair_by_length(self):
         # Згрупуйємо назви міст за довжиною їхніх назв і оберімо чотири такі пари
         self.cities_df = self.cities_df.rdd.groupBy(lambda x: len(x[0])).take(4)
         print(self.cities_df)
 
-
     def sort_descending_length(self):
         # Відсортуємо назви міст у порядку спадання довжини їхніх назв
         self.cities_df = self.cities_df.sort(length("mid_length"), ascending=False)
-
 
     def count_by_length(self):
         # Визначимо, скільки слів існує для кожної довжини слова
@@ -102,8 +89,8 @@ scotland_cities.convert_to_json()
 scotland_cities.filter_rows()
 scotland_cities.rename_column()
 scotland_cities.show_result()
-scotland_cities.delete_row()
-scotland_cities.show_result()
+# scotland_cities.delete_row()
+# scotland_cities.show_result()
 scotland_cities.sort_alphabetically()
 scotland_cities.show_result()
 scotland_cities.filter_and_sort_by_length()
