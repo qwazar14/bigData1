25a166b515044d1c46360a106b4276a09da1e497
qwazar14 <zenez89@gmail.com>
2023-01-22 20:27:27 +0200

    a

--- bd3.py
+++ bd3.py
@@ -1,55 +1,21 @@
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from graphframes import *
+from graphframes import GraphFrame
 
-# Initialize SparkSession
+sc = SparkContext("local", "GraphX Test")
+spark = SparkSession(sc)
 
-spark = SparkSession.builder \
-    .appName("GraphX OOP") \
-    .getOrCreate()
-spark.sparkContext.addPyFile("path-to-graphframes-jar")
+vertices = spark.createDataFrame([("name1", "Petro"), ("name2", "Kostiantyn"), ("name3", "Oleh"), ("name4", "Viktoriia"), ("name5", "Hanna"), ("name6", "Dmytro")], ["id", "name"])
+edges = spark.createDataFrame([("name1", "name2", "is-friend-with"), ("name1", "name3", "wrote-status"), ("name2", "name4", "like-status"), ("name3", "name5", "is-friend-with"), ("name4", "name6", "wrote-status"), ("name5", "name1", "like-status")], ["src", "dst", "type"])
+g = GraphFrame(vertices, edges)
 
-# Define vertices
-vertices = spark.createDataFrame([
-    ("name1", "Petro"),
-    ("name2", "Kostiantyn"),
-    ("name3", "Oleh"),
-    ("name4", "Viktoriia"),
-    ("name5", "Hanna"),
-    ("name6", "Dmytro")
-], ["id", "name"])
+# Get in-degree of each vertex
+g.inDegrees.show()
 
-# Define edges
-edges = spark.createDataFrame([
-    ("name1", "name2", "is-friend-with"),
-    ("name1", "name3", "wrote-status"),
-    ("name2", "name4", "like-status"),
-    ("name2", "name5", "is-friend-with"),
-    ("name3", "name6", "wrote-status")
-], ["src", "dst", "relationship"])
+# Filter vertices with in-degree greater than 1
+g.vertices.filter("inDegree > 1").show()
 
-# Create graph
-myGraph = GraphFrame(vertices, edges)
+# Find the number of "is-friend-with" connections in the graph
+g.edges.filter("type = 'is-friend-with'").count()
 
-# 1.2.2 Get the edges from the newly obtained graph
-print(myGraph.edges.collect())
-
-# 1.2.3 Use the triplets() method to combine vertices and edges based on VertexId.
-print(myGraph.triplets.collect())
-
-# 1.2.5 Determine the number of edges coming out of each vertex
-print(myGraph.aggregateMessages(lambda msg: msg.sendToSrc(1), lambda a, b: a + b).collect())
-
-# 1.2.6 For each vertex number, display its name and the number of edges coming out of it
-print(myGraph.aggregateMessages(lambda msg: msg.sendToSrc(1), lambda a, b: a + b).join(myGraph.vertices).collect())
-
-# 1.2.7 Remove the vertex number, leaving only its name and the number of edges coming out of it
-print(myGraph.aggregateMessages(lambda msg: msg.sendToSrc(1), lambda a, b: a + b).join(myGraph.vertices).map(
-    lambda x: (x[1][1], x[1][0])).collect())
-
-# 1.2.8 show all the names of the vertices (even those from which no edges coming out of it)
-print(myGraph.aggregateMessages(lambda msg: msg.sendToSrc(1), lambda a, b: a + b).rightOuterJoin(myGraph.vertices).map(
-    lambda x: (x[1][1], x[1][0])).collect())
-
-# 1.2.9 Let's clean up the previous listing by getting rid of Some and None.
-print(myGraph.aggregateMessages(lambda msg: msg.sendToSrc(1), lambda a, b: a + b).rightOuterJoin(myGraph.vertices).map(
-    lambda x: (x[1][1], x[1][0].getOrElse(0))).collect())
+sc.stop()
